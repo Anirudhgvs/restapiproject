d7618690f7a84a5a00dbb7758941afba464ce91d
Anirudhgvs <anirudhgvs@gmail.com>
2025-05-19 17:22:55 +0530

    committt

--- src/main/java/com/classroom/restapiproject/HelloWorldController.java
+++ src/main/java/com/classroom/restapiproject/HelloWorldController.java
@@ -1,19 +1,45 @@
 package com.classroom.restapiproject;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/books")
 public class HelloWorldController {
 
-    @ResponseStatus(HttpStatus.BAD_GATEWAY)
-    @GetMapping("/classes")
-    public String getMessage(){
-        return "Java and Spring boot batch says hello from my app!!!";
+    private List<Book> books = new ArrayList<>();
+
+    public HelloWorldController() {
+        // Add some sample books when the controller is created
+        books.add(new Book(1L, "The Hobbit", "J.R.R. Tolkien"));
+        books.add(new Book(2L, "Pride and Prejudice", "Jane Austen"));
     }
 
-    //Get, Put, Post, Delete - Http Methods
+
+    @GetMapping
+    public ResponseEntity<?> getBookById(@PathVariable String author,
+            @RequestParam(value = "id", required = true) Long id) {
+
+        //Checking if the book is present in my array list
+        for (Integer i = 0; i < books.size(); i++){
+            Book book = books.get(i);
+            if(id == book.getId()){
+                return ResponseEntity.ok(book);
+            }
+        }
+        throw new BookNotFoundException(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book createABook(@RequestBody Book bookRequest) {
+        books.add(bookRequest);
+        return bookRequest;
+    }
+
 
 }
